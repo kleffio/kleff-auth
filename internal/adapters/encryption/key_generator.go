b79f37c4afd8c253c4f35d541843c67d58d4654e
installer <installer@gmail.com>
2025-11-05 18:27:52 -0500

    Updated the file structure for better clarity

---
+++ internal/adapters/encryption/key_generator.go
@@ -0,0 +1,19 @@
+package encryption
+
+import (
+	"crypto/ed25519"
+	"log"
+)
+
+func main() {
+	//Generate both private and public keys using the algorithm
+	privKey, pubKey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		log.Fatalf("Error generating Ed25519 keys: %v", err)
+	}
+
+	PrivKey := privKey
+	PubKey := pubKey
+	_ = PrivKey
+	_ = PubKey
+}
